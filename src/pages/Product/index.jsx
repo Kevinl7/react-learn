2bf8f667876a80b30b7bd8f26f1a75628aec0715
高权 <mygao666@gmail.com>
2019-07-02 22:40:04 +0800

    :art: 完善商品功能

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -5,28 +5,57 @@ import './index.less'
 
 export default class Product extends React.Component {
   state = {
-    data: []
+    data: [],
+    parentId: 0
   }
 
   handleAdd = () => {
     
     
   }
+  handleClick = (record) => {
+    this.setState({
+      parentId: record.id
+    }, () => {
+      this.getData(true)
+    })
+  }
 
-  componentDidMount() {
-    let data = [
+  getData = (x) => {
+    let data = !x ? [
       {
         key: '1',
         name: 'John Brown',
         money: '￥300,000.00',
-        address: 'New York No. 1 Lake Park',
+        id: 1
       }
-    ]
+    ] : [
+        {
+          key: '11',
+          name: '儿子',
+          money: '￥300,000.00',
+          id: 11
+        }
+      ]
     this.setState({ data })
   }
 
+  handleRead = (record) => {
+    console.log(record)
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
   render() {
-    let title = '商品分类'
+    let title = this.state.parentId === 0 ? '商品分类'
+      : (
+        <div>
+          <span>商品分类</span> > 
+          <span>erzi </span>
+        </div>
+      )
     let extra = (
       <Button type='primary' onClick={this.handleAdd}><Icon type='plus'></Icon> 添加</Button>
     )
@@ -37,7 +66,12 @@ export default class Product extends React.Component {
       },
       {
         title: '操作',
-        render: (record) => (<div><Button onClick={() => this.handleClick(record)}>详情</Button></div>)
+        render: (record) => (
+          <div>
+            <Button onClick={() => this.handleClick(record) }>详情</Button>
+            <Button onClick={this.handleRead}>查看二级</Button>
+          </div>
+        )
       },
     ];
 
